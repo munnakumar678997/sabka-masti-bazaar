8691d187f9f85d0a60169f78a16fb46387f62595
munnakumar678997 <munnakumar678997@gmail.com>
2026-06-23 10:28:20 +0530

    🛣️ /store route add kiya

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Loading from './pages/Loading';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Store from './pages/Store';
 import './styles/global.css';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/loading" element={<Loading />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/store" element={<Store />} />
         </Routes>
       </div>
     </BrowserRouter>
